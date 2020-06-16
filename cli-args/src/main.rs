6912ffbe3fa89dd715b0e7e157773c294426502b
Russ R <russryba@gmail.com>
2020-06-16 01:25:46 -0400

    Working on CLI args that have to be picked from a list of values.

--- cli-args/src/main.rs
+++ cli-args/src/main.rs
@@ -1,6 +1,8 @@
 use std::thread;
 use structopt::StructOpt;
 
+// This is copied and pasted willy-nilly from Rust CLI book
+// Just messing around with StructOpts 
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -12,6 +14,7 @@ struct CliOptions {
     debug: bool,
 
     /// The pattern to look for
+    #[structopt(default_value="SHA256")]
     hash_method: String,
 
     /// The path to the file to read
@@ -32,10 +35,10 @@ use exitfailure::ExitFailure;
 // }
 
 fn do_hard_work() {
-    // for i in 1..123 {
-    //     let x = 1;
-    // }
-    thread::sleep_ms(300);
+    for i in 1..123 {
+        let x = 1;
+    }
+    
 
 }
 
@@ -44,13 +47,18 @@ fn main() {
     let opt = CliOptions::from_args();
     println!("Debug: {:?}, Hash Method: {:?}, Path: {:?}", opt.debug, opt.hash_method, opt.path);
     let known_hash_methods = ["SHA256", "MD5", "SHA1"];
-    if known_hash_methods.contains(&&opt.hash_method[..]) {
+    if known_hash_methods.contains(&&String::from(opt.hash_method).to_uppercase()[..]) {
         println!("Known Hash Method: {:?}", opt.hash_method);
     } else {
         println!("Please Use a Known Hash Method: {:?}", known_hash_methods)
     }
     for i in 0..10 {
-        do_hard_work();
+        // do_hard_work();
+        for i in 1..5 {
+            pb.set_message(&format!("{}: {}", "Working...", i));
+            thread::sleep_ms(30);
+        }
+        
         pb.println(format!("[+] finished #{}", i));
         pb.inc(1);
     }
