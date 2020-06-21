f0eb89db32fd21b538057c344221de7837f6b464
Russ R <russryba@gmail.com>
2020-06-21 00:10:55 -0400

    This example works... start from this.

--- people-in-space/src/main.rs
+++ people-in-space/src/main.rs
@@ -1,13 +1,22 @@
+use reqwest::Result;
+use std::time::Duration;
+use reqwest::ClientBuilder;
 
-fn main() {
-    let body = reqwest::get("https://www.rust-lang.org")
-    .await?
-    .text()
-    .await?;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let user = "ferris-the-crab";
+    let request_url = format!("https://api.github.com/users/{}", user);
+    println!("{}", request_url);
 
-    println!("body = {:?}", body);
+    let timeout = Duration::new(5, 0);
+    let client = ClientBuilder::new().timeout(timeout).build()?;
+    let response = client.head(&request_url).send().await?;
 
-    // let response = requests::get("http://api.open-notify.org/astros.json").unwrap();
-    // let data = response.json().unwrap();
-    // println!("{:?}", data);
+    if response.status().is_success() {
+        println!("{} is a user!", user);
+    } else {
+        println!("{} is not a user!", user);
+    }
+
+    Ok(())
 }
