07ff057049a2f8c802d2ffd5111c5624b3bc6b0a
Russ R <russryba@gmail.com>
2020-06-20 23:25:52 -0400

    This didn't work either...

--- people-in-space/src/main.rs
+++ people-in-space/src/main.rs
@@ -1,20 +1,13 @@
-extern crate requests;
-use requests::ToJson;
 
 fn main() {
-    let response = requests::get("http://httpbin.org/get").unwrap();
-    assert_eq!(response.url(), "http://httpbin.org/get");
-    assert_eq!(response.reason(), "OK");
-    assert_eq!(response.status_code(), requests::StatusCode::Ok);
+    let body = reqwest::get("https://www.rust-lang.org")
+    .await?
+    .text()
+    .await?;
 
-    let data = response.json().unwrap();
-    assert_eq!(data["url"], "http://httpbin.org/get");
-    assert_eq!(data["headers"]["Host"], "httpbin.org");
-    assert_eq!(data["headers"]["User-Agent"],
-               concat!("requests-rs/", env!("CARGO_PKG_VERSION")));
+    println!("body = {:?}", body);
 
-
-    let response = requests::get("http://api.open-notify.org/astros.json").unwrap();
-    let data = response.json().unwrap();
-    println!("{:?}", data);
+    // let response = requests::get("http://api.open-notify.org/astros.json").unwrap();
+    // let data = response.json().unwrap();
+    // println!("{:?}", data);
 }
