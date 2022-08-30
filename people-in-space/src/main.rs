f47656a018149d211a44fe08b2a5d414d6299d41
Russ R <russryba@gmail.com>
2022-08-29 22:05:11 -0400

    Updated program to work with modern rust

--- people-in-space/src/main.rs
+++ people-in-space/src/main.rs
@@ -1,30 +1,16 @@
-use reqwest::Result;
-use std::time::Duration;
-use reqwest::ClientBuilder;
+use std::error::Error;
 
 #[tokio::main]
-async fn main() -> Result<()> {
-    let user = "ferris-the-crab";
-    let request_url = format!("https://api.github.com/users/{}", user);
-    println!("{}", request_url);
+async fn main() -> Result<(), Box<dyn Error>> {
+    let people_in_space = "http://api.open-notify.org/astros.json";
+    let request_url = people_in_space;
 
-    let timeout = Duration::new(5, 0);
-    let client = ClientBuilder::new().timeout(timeout).build()?;
-    let response = client.head(&request_url).send().await?;
 
-    let x: i32 = 0;
-    let y: i32 = 0;
-    let x1: i32 = 5;
-    let y1: i32 = 5;
-    let c: f64 = (y1-y) as f64;
-    let d = c.atan2((x1-x) as f64);
-    println!("{}", d.to_degrees());
-
-    if response.status().is_success() {
-        println!("{} is a user!", user);
-    } else {
-        println!("{} is not a user!", user);
-    }
+    let resp = reqwest::get(request_url)
+        .await?
+        .text()
+        .await?;
+    println!("{}", resp);
 
     Ok(())
 }
